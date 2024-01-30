cd30dd528d61e914fa2206e368d093d96b2adef5
thon1525 <git@github.com>
2024-01-30 13:56:56 +0700

    add this page

--- kru-app/src/app/page.tsx
+++ kru-app/src/app/page.tsx
@@ -2,12 +2,17 @@ import { Button } from "@/components";
 import React from "react";
 
 const Homepage = () => {
+  const handleClick = () => {
+    console.log("Button clicked!");
+    // Add your logic here
+  };
   return (
     <div>
       <Button
         isDisabled={true}
         colorScheme="secondary"
         className="border rounded-full px-4 py-2 w-[100px]"
+     
       >
         Hello
       </Button>
