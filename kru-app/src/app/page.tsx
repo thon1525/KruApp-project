6ddd773ad2a01d79fb5368b227668f9bee461f16
thon1525 <git@github.com>
2024-01-31 10:18:56 +0700

    Your commit message

--- kru-app/src/app/page.tsx
+++ kru-app/src/app/page.tsx
@@ -1,5 +1,5 @@
 
-import { Button, Typography } from "@/components";
+import { Button, Dropdown, Typography } from "@/components";
 import React from "react";
 const Homepage = () => {
   const handleClick = () => {
@@ -23,6 +23,20 @@ const Homepage = () => {
       >
         សួស្ដីប្រូ !
       </Typography>
+
+      <Dropdown className="my-custom-dropdown left-1/2 text-sm">
+      <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
+        <div className="py-1" role="none">
+          
+          <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-1">Duplicate</a>
+        </div>
+        <div className="py-1" role="none">
+          <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-2">Edit</a>
+         
+        </div>
+      </div>
+    </Dropdown>
+
     </div>
   );
 };
